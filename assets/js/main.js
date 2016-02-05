30cfb6213a528d61be27582d97380336c2d82ba8
Oli Cundale <ocundale@gmail.com>
2016-02-05 12:43:20 +0000

    fixes to bottom

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,7 +113,7 @@
 					$message;
 
 			// Bail if addEventListener isn't supported.
-				if (!('addEventListener' in $form))
+				if (!$form || !('addEventListener' in $form))
 					return;
 
 			// Message.
